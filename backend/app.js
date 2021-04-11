4e79feb8b0eb0894d0f36b53dbae9acfea62dbb5
loic-demanou <loicdemanou@icloud.com>
2021-04-11 18:23:26 +0100

    course crud

--- backend/app.js
+++ backend/app.js
@@ -2,6 +2,7 @@ const express = require('express');
 
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const Thing = require('./models/Thing');
 
 const app = express();
 
@@ -24,31 +25,41 @@ app.use((req, res, next) => {
 app.use(bodyParser.json());
 
 app.post('/api/stuff', (req, res, next) => {
-    res.status(201).json({
-        message: 'objet crée'
+    delete req.body._id;
+    const thing = new Thing({
+        ...req.body
     });
+    thing.save()
+        .then(() => res.status(201).json({ message: 'Saved done !' }))
+        .catch(error => res.status(400).json({ error }));
 });
 
-app.use('/api/stuff', (req, res, next) => {
-    const stuff = [
-        {
-            _id: 'oeihfzeoi',
-            title: 'Mon premier objet',
-            description: 'Les infos de mon premier objet',
-            imageUrl: 'https://cdn.pixabay.com/photo/2019/06/11/18/56/camera-4267692_1280.jpg',
-            price: 4900,
-            userId: 'qsomihvqios',
-        },
-        {
-            _id: 'oeihfzeomoihi',
-            title: 'Mon deuxième objet',
-            description: 'Les infos de mon deuxième objet',
-            imageUrl: 'https://cdn.pixabay.com/photo/2019/06/11/18/56/camera-4267692_1280.jpg',
-            price: 2900,
-            userId: 'qsomihvqios',
-        },
-    ];
-    res.status(200).json(stuff);
+app.get('/api/stuff', (req, res, next) => {
+    Thing.find()
+        .then(things => res.status(200).json(things))
+        .catch(error => res.status(400).json({ error }));
 });
+
+app.get('/api/stuff/:id', (req, res, next) =>{
+    Thing.findOne({_id: req.params.id})
+    .then(things => res.status(200).json(things))
+    .catch(error => res.status(400).json({ error }));
+
+});
+
+app.put('/api/stuff/:id', (req, res, next)=>{
+    Thing.updateOne({_id:req.params.id}, {...req.body, _id:req.params.id})
+    .then(() => res.status(201).json({ message: 'updated done !' }))
+    .catch(error => res.status(400).json({ error }));
+});
+
+app.delete('/api/stuff/:id', (req, res, next)=>{
+    Thing.deleteOne({_id: req.params.id})
+    .then(() => res.status(201).json({ message: 'deleted !' }))
+    .catch(error => res.status(400).json({ error }));
+
+});
+
+
 
 module.exports = app;
